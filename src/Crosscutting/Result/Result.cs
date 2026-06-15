d000d44d43dfcacf2859a00eaa9730b105e37810
aidangannon <aidanwilliamgannon@gmail.com>
2026-06-15 11:12:54 +0100

    Add result pattern

--- src/Crosscutting/Result/Result.cs
+++ src/Crosscutting/Result/Result.cs
@@ -1,8 +1,61 @@
-namespace i;
+namespace Crosscutting.Result;
 
 public class Result
 {
-    public string Error { get; init; }
+    public required string? Error { get; init; }
 
-    public static Result
+    public static Result Ok()
+    {
+        return new Result
+        {
+            Error = null
+        };
+    }
+
+    public static Result Err(string message)
+    {
+        return new Result
+        {
+            Error = message
+        };
+    }
+
+    public bool HasFailed()
+    {
+        return Error == null;
+    }
+}
+
+public class Result<T>
+{
+    public required string? Error { get; init; }
+    public required T? Value { get; init; }
+
+    public static Result<T> Ok(T value)
+    {
+        return new Result<T>
+        {
+            Error = null,
+            Value = value
+        };
+    }
+
+    public static Result<T> Err(string message)
+    {
+        return new Result<T>
+        {
+            Error = message,
+            Value = default
+        };
+    }
+
+    public bool HasFailed()
+    {
+        return Error == null;
+    }
+
+    public T Unwrap()
+    {
+        return Value ?? throw new ArgumentNullException(nameof(Value), "Result value is null, failed to unwrap");
+    }
 }
